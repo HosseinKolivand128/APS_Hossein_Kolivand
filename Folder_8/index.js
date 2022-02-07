ceac8b117f58172b7559c1a18300fec1b83526b9
HosseinKolivand128 <ac.hossein.ac@gmail.com>
2022-02-07 13:09:57 +0330

    Updated

---
+++ Folder_8/index.js
@@ -0,0 +1,11 @@
+// const div = document.createElement("div");
+// div.className = "planet";
+// // div.classList.add("planet");
+// // console.log(div.classList);
+// div.style.backgroundColor = "blue";
+// document.body.append(div);
+
+const div = document.createElement("div");
+div.className = "planet";
+div.style.backgroundColor = "blue";
+document.body.appendChild(div);
