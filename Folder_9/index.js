c1bf55f4675a9b1ec9a77c795cbd5365625a74aa
HosseinKolivand128 <ac.hossein.ac@gmail.com>
2022-02-11 18:25:15 +0330

    Update index.js

---
+++ Folder_9/index.js
@@ -0,0 +1,11 @@
+var myCuteCat = {
+  name: "Angel",
+  age: 3,
+  isFeisty: true,
+  likes: ["rubber bands", "boxes", "4am petting sessions"],
+  fur: { colors: ["orange", "white"], pattern: "striped" },
+  meow: function () {
+    log("MEOOOWWW!");
+  },
+};
+console.log(myCuteCat.meow());
